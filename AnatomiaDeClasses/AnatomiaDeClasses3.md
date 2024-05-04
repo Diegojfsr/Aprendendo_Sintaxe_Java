93dd4a502a3344ad548bb8b042f1cfa50059a7be
DiegoJfsr <diegojfsr@gmail.com>
2024-05-03 21:51:38 -0300

    Anatomia de classes 3

---
+++ AnatomiaDeClasses/AnatomiaDeClasses3.md
@@ -0,0 +1,27 @@
+
+Anatomia de classes III
+
+Declarando Variaveis e Metodos
+Existe uma estrutura comum para ambas.
+
+Declarando Variaveis.
+Para declarar uma variavel em Java, sempre vamos seguir essa estrutura.
+
+Tipo NomeBemDefinido = Atribuicao(Opcionalem alguns casos)
+Exemplo:
+
+int idade = 23;
+double altura = 1.62;
+Dog spike; // aqui a variavel ainda nao tem nenhum valor.
+
+Declarando metodos.
+Para declarar um metodo em Java, sempre vamos seguir essa estrutura.
+
+TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+Exemplo:
+int somar (int numeroUm, int numero2)
+String formatarCep (long cep)
+
+
+
--- AnatomiaDeClasses/src/MinhaClasse.java
+++ AnatomiaDeClasses/src/MinhaClasse.java
@@ -7,9 +7,32 @@ public class MinhaClasse {
 
         //ANCII
         int ano = 2021;
-        //ano = 2022;
+        //ano = 2022; // variavel nao pode ter o valor modificado
 
         final String BR = "Brasil";
-        //BR = "Brazil";
+        //BR = "Brazil"; // variavel nao pode ter o valor modificado
+
+        //ANCIII
+        //Declarando Variaveis
+        String meuNome = "Diego";
+        int anoFabricacao = 2022;
+        boolean verdadeira = true;
+        //anoFrabricacao = 2018; // variavel recebe um novo valor
+
+        
+        // Declarando Metodos
+        String primeiroNome = "Diego";
+        String segundoNome = "Jefferson";
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
     }
+    // Declarando Metodos
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do Metodo " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+
+
+
+
 }
